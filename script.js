271e41b93f35cf7196a945e9c41e26a172d91f1c
JustPhvn <justicequyphan@gmail.com>
2020-02-12 18:36:10 -0700

    Set time at top, timeslots change color based on current time

--- script.js
+++ script.js
@@ -11,3 +11,29 @@
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
+
+var currentTime;
+var currentHour = moment().format("H");
+
+//loads time at top of page & refreshes
+setInterval(function() {
+  currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
+  $("#currentDay").text(currentTime);
+}, 999);
+
+//sets color for input blocks
+// console.log(currentHour);
+for (let i = 9; i < 18; i++) {
+  let timeSlot = $("#" + i);
+  //   console.log(timeSlot);
+  if (i === currentHour) {
+    timeSlot.addClass("present");
+  } else if (i < currentHour) {
+    timeSlot.addClass("past");
+  } else {
+    timeSlot.addClass("future");
+  }
+  $("." + i).on("click", "button", function() {
+    timeSlot.value;
+  });
+}
