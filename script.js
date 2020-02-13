71c6261a7c2bf823b4aed195541e69bbced7dd28
JustPhvn <justicequyphan@gmail.com>
2020-02-13 14:18:40 -0700

    Minor edits to get text areas to properly work

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
 
 var currentTime;
 var currentHour = moment().format("H");
-console.log(currentHour);
+// console.log(currentHour);
 
 //loads time at top of page & refreshes
 setInterval(function() {
@@ -24,27 +24,47 @@ setInterval(function() {
 
 //sets color for input blocks
 // console.log(currentHour);
-var calArray = [];
 for (let i = 9; i < 18; i++) {
   let timeSlot = $("#" + i);
-  // console.log(calItem);
-  console.log(i);
   if (i == currentHour) {
     timeSlot.removeClass("past future").addClass("present");
   } else if (i < currentHour) {
     timeSlot.removeClass("present future").addClass("past");
   } else {
     timeSlot.removeClass("past present").addClass("future");
-    $("." + i).on("click", function() {
-      var calItem = timeSlot.val();
-      calArray.push(calItem);
-      //   // console.log(calArray);
-      //   // console.log(calItem);
-      localStorage.setItem("calendarItem", JSON.stringify(calArray));
-    });
   }
+  $("." + i).on("click", function() {
+    console.log(this);
+  });
 }
-// let loadItems = localStorage.getItem("calendarItem");
-// calArray = JSON.parse(loadItems);
-// console.log(calArray);
-// timeSlot.val(localStorage.getItem("calendarItem"));
+//make an object and target text area
+
+// for (let j = 0; j < 9; j++) {
+//   for (let i = 9; i < 18; i++) {
+//     $("." + i).on("click", function() {
+//       let timeSlot = $("#" + i);
+//       // alert("button clicked");
+//       var calArray = [
+//         "9AM",
+//         "10AM",
+//         "11AM",
+//         "12PM",
+//         "1PM",
+//         "2PM",
+//         "3PM",
+//         "4PM",
+//         "5PM"
+//       ];
+//       var calItem = timeSlot.val();
+//       // console.log(this);
+//       calArray[j] = calItem;
+//       console.log(calArray);
+//       //   // console.log(calItem);
+//       localStorage.setItem("calendarItem", JSON.stringify(calArray));
+//     });
+//     // let loadItems = localStorage.getItem("calendarItem");
+//     // calArray = JSON.parse(loadItems);
+//     // console.log(calArray);
+//     // timeSlot.val(localStorage.getItem("calendarItem"));
+//   }
+// }
